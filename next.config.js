ad9c9a3ae14360fc45c518cd253e247f712a00a0
Erlan <99065164+ERRRRRRRLAN@users.noreply.github.com>
2025-11-20 12:45:41 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Ensure API routes work correctly
+  async rewrites() {
+    return []
+  },
 }
 
 module.exports = nextConfig
