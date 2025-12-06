347a6e897f17bfc823a96d0d05582a486a9e20d6
FazaSPT <evriliantiniandrian7@gmail.com>
2025-12-06 19:14:46 +0700

    fix: perbaiki import path di logout.ts - gunakan relative path yang benar

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '../[...nextauth]'
+import { authOptions } from './[...nextauth]'
 
 export default async function handler(
   req: NextApiRequest,
